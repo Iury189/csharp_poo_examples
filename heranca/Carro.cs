e0ef28400aba26e98a949407ce567153047997cd
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-03-24 11:00:57 -0300

    Update Carro.cs

--- heranca/Carro.cs
+++ heranca/Carro.cs
@@ -10,8 +10,8 @@ namespace heranca {
 
             var hora_atual = DateTime.Now;
             WriteLine("\n");
-            WriteLine($"Modelo: {modelo}");
             WriteLine($"Marca: {marca}");
+            WriteLine($"Modelo: {modelo}");
             WriteLine($"Cor: {cor}");
             WriteLine($"Ano de fabricação: {ano}");
             WriteLine($"Valor: {valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))}");
