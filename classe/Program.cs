86cbd594376ea64aaee0a01596e55550b7298e97
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-03-24 11:02:45 -0300

    Update Program.cs

--- classe/Program.cs
+++ classe/Program.cs
@@ -8,9 +8,9 @@ namespace classe {
 
             JoJo j1 = new JoJo();
             Write("Digite o nome do usuário: ");
-            j1.setNome(ReadLine());
-            Write($"Digite o nome do stand de {j1.getNome()}: ");
-            j1.setStand(ReadLine());
+            j1.nome = ReadLine()
+            Write($"Digite o nome do stand de {j1.nome}: ");
+            j1.stand = ReadLine()
             Write("\n");
             j1.interacao();
             ReadKey();
