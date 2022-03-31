42bb5a4522c55f47422b1fb7f0c80e69f5157694
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-03-31 09:55:37 -0300

    Update JoJo.cs

--- classe/JoJo.cs
+++ classe/JoJo.cs
@@ -1,47 +1,54 @@
 ﻿using static System.Console;
 
-namespace classe {
-
-    public class JoJo {
-
+namespace classe 
+{
+    public class JoJo 
+    {
         public string nome { get; set; }
         public string stand { get; set; }
         public bool status_stand = false;
         
-        public void ativarStand() {
-            if (status_stand) {
+        public void ativarStand() 
+        {
+            if (status_stand) 
+            {
                 WriteLine($"{stand} já está invocado.");
             }
-            else {
+            else 
+            {
                 status_stand = true;
                 WriteLine($"{stand} foi invocado.");
             }
         }
 
-        public void desativarStand() {
-            if (status_stand == false) {
+        public void desativarStand() 
+        {
+            if (status_stand == false) 
+            {
                 WriteLine($"{stand} já está oculto.");
             }
-            else {
+            else 
+            {
                 status_stand = false;
                 WriteLine($"{stand} foi ocultado.");
             }
         }
 
-        public void interacao() {
-            
+        public void interacao() 
+        {    
             string resultado;
             WriteLine($"Nome: {nome}");
             WriteLine($"Stand: {stand}");
             resultado = status_stand ? "Ativo." : "Inativo.";
             WriteLine($"Status do stand: {resultado}");
-            if (status_stand) {
+            if (status_stand) 
+            {
                 ativarStand();
             }
-            else {
+            else 
+            {
                 desativarStand();
             }
-            
         }
     }
 }
