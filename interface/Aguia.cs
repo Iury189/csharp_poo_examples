c27d8a8c0862c9b5b4d3b0f865f5bdbc00e6cacf
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-03-31 09:59:44 -0300

    Update Aguia.cs

--- interface/Aguia.cs
+++ interface/Aguia.cs
@@ -1,10 +1,11 @@
 ﻿using static System.Console;
 
-namespace iinterface {
-
-    public class Aguia : Ipredador {
-
-        public void cacar() {
+namespace iinterface 
+{
+    public class Aguia : Ipredador 
+    {
+        public void cacar() 
+        {
             WriteLine("A águia está caçando o sapo.");
         }
     }
