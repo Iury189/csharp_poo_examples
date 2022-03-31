163b27019a646c6f6aa6f6499ab60f39cdf0c87f
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-03-31 09:42:59 -0300

    Update Program.cs

--- classe/Program.cs
+++ classe/Program.cs
@@ -4,17 +4,16 @@ namespace classe {
 
     class MainClass {
 
-        public static void Main(string[] args){
-
+        public static void Main(string[] args)
+        {
             JoJo j1 = new JoJo();
             Write("Digite o nome do usuário: ");
-            j1.nome = ReadLine()
+            j1.nome = ReadLine();
             Write($"Digite o nome do stand de {j1.nome}: ");
-            j1.stand = ReadLine()
+            j1.stand = ReadLine();
             Write("\n");
             j1.interacao();
-            ReadKey();
-            
+            ReadKey();   
         }
     }
 }
