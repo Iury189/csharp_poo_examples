06b8e5c7a4173961f434c9d729d85a2532136ad5
Iury189 <iury.fernandes@protonmail.com>
2023-07-09 12:15:05 -0300

    Third commit

--- classe/Program.cs
+++ classe/Program.cs
@@ -33,6 +33,58 @@ namespace classe
                     break;
                 }
             }
+            while (true)
+            {
+                try 
+                {
+                    if (j1.status_stand == false)
+                    {
+                        WriteLine($"O stand {j1.stand} está desativado.");
+                        Write("Deseja ativar? (0 - Não | 1 - Sim): ");
+                        int resposta_status = int.Parse(ReadLine());
+                        if (resposta_status == 1)
+                        {
+                            status_stand = true;
+                            WriteLine($"O Stand {j1.stand} foi ativado.");
+                            break;
+                        } 
+                        else if (resposta_status == 0)
+                        {
+                            WriteLine($"O Stand {j1.stand} continuará inativo.");
+                            break;
+                        } 
+                        else 
+                        {
+                            WriteLine("Opção inválida, digite uma das respectivas opções válidas.");
+                        }
+                    } 
+                    else 
+                    {
+                        WriteLine($"O stand {j1.stand} está ativado.");
+                        Write("Deseja desativar? (0 - Não | 1 - Sim): ");
+                        int resposta_status = int.Parse(ReadLine());
+                        if (resposta_status == 1)
+                        {
+                            status_stand = false;
+                            WriteLine($"O Stand {j1.stand} foi desativado.");
+                            break;
+                        } 
+                        else if (resposta_status == 0)
+                        {
+                            WriteLine($"O Stand {j1.stand} continuará ativo.");
+                            break;
+                        } 
+                        else 
+                        {
+                            WriteLine("Opção inválida, digite uma das respectivas opções.");
+                        }
+                    }
+                } 
+                catch (Exception e) 
+                {
+                    WriteLine("Campo deve conter apenas números inteiros, refaça a operação.");
+                }
+            }
             j1.interacao();
         }
     }
